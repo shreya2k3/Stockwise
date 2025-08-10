fda1aa11e674e7dbb47a7433b4469b4d65c01faf
Shreya Singh <shreyasingh2k3@gmail.com>
2025-08-10 23:13:48 +0530

    Delete BlackRock/Hack directory

--- BlackRock/Hack/App/management/commands/run_flask.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = 'Run Flask app'
-
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Starting Flask app...'))
-        subprocess.Popen(['python', 'flask_app\app.py'])
